0ddd23d2aa5ec64465cdb1f35f578232765ece7d
TheRichDarth <73854618+TheRichDarth@users.noreply.github.com>
2023-11-28 15:40:00 -0500

    Began piece class definition
    Next thing to do is finish the class and then extend it for each piece

--- board.js
+++ board.js
@@ -14,9 +14,30 @@ class Tile {
 
     }
 }
+/**
+ * Identifiers for different piece teams
+ */
+Team = {
+    white,black
+}
 
+/**
+ * In this implementation, each piece stores its location, size, team and type. Multi-tile pieces use their first index on each axis for their position
+ */
 class Piece {
-    constructor(){
-        
+    /**
+     * @param {int} x index of the piece's i location; The index of the first tile the piece occupies
+     * @param {int} y index of the piece's j location; The index of the first tile the piece occupies
+     * @param {int} width the width of the piece in tiles
+     * @param {int} height the height pf the piece in tiles
+     * @param {Team} team the team of the piece using the team object. Team.white and Team.black are the common teams
+     * @param {String} pieceName 
+     */
+    constructor(x, y, width,height,team){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.team = team;
     }
 }
