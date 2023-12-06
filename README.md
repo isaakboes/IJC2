da991bda0568a542b2d62564c9334062063deec9
TheRichDarth <73854618+TheRichDarth@users.noreply.github.com>
2023-12-05 22:35:47 -0500

    Finished piece class and added move class

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # IJC2
-This is It's Just Chess: The Most Chaotic Game of Chess you'll every play
+## This is It's Just Chess: The Most Chaotic Game of Chess you'll every play
 
-This JS-DOM game takes chess and adds fun and interesting mechanics. The beta version can be played at rpgrat.org/chess
+**This JS-DOM game takes chess and adds fun and interesting mechanics. The beta version can be played at rpgrat.org/chess**
+
--- board.js
+++ board.js
@@ -22,7 +22,50 @@ Team = {
 }
 
 /**
+ * This class stores a possible move a piece can make
+ */
+class Move {
+    /**
+     * This constructor is for all pieces.
+     * @param {int} dx The horizontal motion of the move
+     * @param {int} dy The vertical motion of the move
+     * @param {boolean} repeated Can the move be repeated infinitely (i.e. bishops or rooks moves can be repeated until they run into something)
+     */
+    constructor(dx,dy,repeated){
+        this.dx = dx;
+        this.dy = dy;
+        this.repeated = repeated;
+    }
+    /**
+     * This constructor is for all pieces.
+     * @param {int} dx The horizontal motion of the move
+     * @param {int} dy The vertical motion of the move
+     * This move isn't repeated
+     */
+    constructor(dx,dy){
+        this.dx = dx;
+        this.dy = dy;
+        this.repeated = false;
+    }
+    /**
+     * Gets the dx variable of this move
+     * @returns Change in horizontal position
+     */
+    getDx(){
+        return this.dx;
+    }
+    /**
+     * Gets the dy variable of this move
+     * @returns Change in vertical position
+     */
+    getDy(){
+        return this.dy;
+    }
+}
+
+/**
  * In this implementation, each piece stores its location, size, team and type. Multi-tile pieces use their first index on each axis for their position
+ * 
  */
 class Piece {
     /**
@@ -31,13 +74,95 @@ class Piece {
      * @param {int} width the width of the piece in tiles
      * @param {int} height the height pf the piece in tiles
      * @param {Team} team the team of the piece using the team object. Team.white and Team.black are the common teams
-     * @param {String} pieceName 
+     * @param {String} pieceName the name of the piece (For example, tyler)
+     * @param {Move[]} moves the list of moves the piece can move in
      */
-    constructor(x, y, width,height,team){
-        this.x = x;
-        this.y = y;
+    constructor(x, y, width,height,team, moves){
+        this.xPosition = x;
+        this.yPosition = y;
         this.width = width;
         this.height = height;
         this.team = team;
+        this.moves = moves;
     }
+    /**
+     * Gets the piece horizontal position
+     * @returns Piece's x position
+     */
+    getX(){
+        return this.xPosition;
+    }
+    /**
+     * Gets the piece vertical position
+     * @returns Piece's y position
+     */
+    getY(){
+        return this.yPosition;
+    }
+    /** Gets the width of the piece
+     * @returns piece width in board tiles
+     */
+    getWidth(){
+        return this.width;
+    }
+    /** Gets the height of the piece
+     * @returns piece height in board tiles
+     */
+    getHeight(){
+        return this.height;
+    }
+    /** Gets the piece team
+     * @returns {Team} the piece team
+     */
+    getTeam(){
+        return this.team;
+    }
+    /**
+     * Returns the moves of the piece
+     * @returns {Move[]} The piece's move list
+     */
+    getMoves(){
+        return this.moves;
+    }
+
+    /**
+     * Gets the first X index that this piece occupies
+     * @return {int} The x index of the first tile that this piece occupies
+     */
+    getFirstXIndex(){
+        return this.x;
+    }
+    /**
+     * Gets the first Y index that this piece occupies
+     * @return {int} The y index of the first tile that this piece occupies
+     */
+    getFirstYIndex(){
+        return this.y;
+    }
+    /**
+     * Gets the last X index that this piece occupies
+     * @return {int} The x index of the last tile that this piece occupies
+     */
+    getLastXIndex(){
+        return this.x+this.width-1;
+    }
+    /**
+     * Gets the last Y index that this piece occupies
+     * @return {int} The y index of the last tile that this piece occupies
+     */
+    getLastYIndex(){
+        return this.y+this.height-1;
+    }
+    
+
+    /**
+     * Moves a piece in accordance with a move from a given index.
+     * @param {int} index The index of the move to move the piece with
+     */
+    movePiece(index){
+        this.x += this.moves[index].getDx();
+        this.Y += this.moves[index].getDY();
+        
+    }
+    
 }
